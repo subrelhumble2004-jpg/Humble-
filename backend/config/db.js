415dcb58db966f5a35ecea37cba903ede53523c2
Onu Confidence <subrelhumble2004@gmail.com>
2026-08-12 04:02:05 +0100

    Update db.js
    Fix MySQL database connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,11 +7,9 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-
   dateStrings: true,
   connectTimeout: 10000
 });
